e6e489e7832af9924a986c0b4538a74bc0aa185d
Gemma <johndoe@example.com>
2023-11-23 12:32:14 +0100

    menu responsive

--- frontend/src/components/NavBar/Navbar.js
+++ frontend/src/components/NavBar/Navbar.js
@@ -160,17 +160,26 @@ function Navbar () {
       <nav className='nav4'>
         <button className='buttonMenu' onClick={handleMenu}>
           <img className='menuLogo' src='/../menu.png' alt='menuIcon'></img>
-          <img className='menuLogoHover' src='/../menu2.png' alt='menuIconHover'></img>
+          <img
+            className='menuLogoHover'
+            src='/../menu2.png'
+            alt='menuIconHover'
+          ></img>
         </button>
 
         {openMenu && (
-          <div>
+          <div className='menuContainer'> 
             <div class='icon'>
-              <img className='arrowUpIcon' src='/../arrow-up2.png' alt='arrowUp2'></img>
+              <img
+                className='arrowUpIcon'
+                src='/../arrow-up2.png'
+                alt='arrowUp2'
+              ></img>
             </div>
             <div className='menuNav'>
               <NavLink
-                to='/members' onClick={handleMenu}
+                to='/members'
+                onClick={handleMenu}
                 className={({ isActive }) =>
                   isActive ? 'Members2' : 'Members'
                 }
@@ -181,7 +190,8 @@ function Navbar () {
               {!isLoggedIn && (
                 <NavLink
                   exact
-                  to='/sign-in' onClick={handleMenu}
+                  to='/sign-in'
+                  onClick={handleMenu}
                   className={({ isActive }) =>
                     isActive ? 'Sign-in2' : 'Sign-in'
                   }
@@ -197,7 +207,147 @@ function Navbar () {
               {!isLoggedIn && (
                 <NavLink
                   exact
-                  to='/sign-in-noLS' onClick={handleMenu}
+                  to='/sign-in-noLS'
+                  onClick={handleMenu}
+                  className={({ isActive }) =>
+                    isActive ? 'Sign-in5' : 'Sign-in6'
+                  }
+                >
+                  <h1>Log in</h1>
+                </NavLink>
+              )}
+
+              {isLoggedIn && (
+                <NavLink
+                  exact
+                  to='/members'
+                  className={({ isActive }) =>
+                    isActive ? 'Members4' : 'Members3'
+                  }
+                >
+                  <h1>Network members</h1>
+                </NavLink>
+              )}
+
+              {isLoggedIn && (
+                <NavLink
+                  exact
+                  to='/individuals'
+                  className={({ isActive }) =>
+                    isActive ? 'Sign-in4' : 'Sign-in3'
+                  }
+                  onClick={handleClik}
+                >
+                  <img
+                    className='ls-login-image2'
+                    src='/../logout.png'
+                    alt='ls-login-image2'
+                  />
+                  <h1>Log out</h1>
+                </NavLink>
+              )}
+            </div>
+            <div className='menuNav2'>
+              <NavLink
+                exact
+                to='/'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Individuals2' : 'Individuals'
+                }
+              >
+                <h1>Individuals</h1>
+              </NavLink>
+              <NavLink
+                exact
+                to='/biosamples'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Biosamples2' : 'Biosamples'
+                }
+              >
+                <h1>Biosamples</h1>
+              </NavLink>
+              <NavLink
+                exact
+                to='/genomicVariations'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Variants2' : 'Variants'
+                }
+              >
+                <h1>Variant</h1>
+              </NavLink>
+              <NavLink
+                exact
+                to='/runs'
+                onClick={handleMenu}
+                className={({ isActive }) => (isActive ? 'Runs2' : 'Runs')}
+              >
+                <h1>Runs</h1>
+              </NavLink>
+              <NavLink
+                exact
+                to='/analyses'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Analyses2' : 'Analyses'
+                }
+              >
+                <h1>Analyses</h1>
+              </NavLink>
+              <NavLink
+                exact
+                to='/cohorts'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Cohorts2' : 'Cohorts'
+                }
+              >
+                <h1>Cohorts</h1>
+              </NavLink>
+              <NavLink
+                exact
+                to='/cross-queries/allScopes/%20/'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Cross-queries2' : 'Cross-queries'
+                }
+              >
+                <h1>Cross queries</h1>
+              </NavLink>
+              <NavLink
+                to='/members'
+                onClick={handleMenu}
+                className={({ isActive }) =>
+                  isActive ? 'Members2' : 'Members'
+                }
+              >
+                {' '}
+                <h1>Network members</h1>
+              </NavLink>
+              {!isLoggedIn && (
+                <NavLink
+                  exact
+                  to='/sign-in'
+                  onClick={handleMenu}
+                  className={({ isActive }) =>
+                    isActive ? 'Sign-in2' : 'Sign-in'
+                  }
+                >
+                  {' '}
+                  <img
+                    className='ls-login-image'
+                    src='/../ls-login.png'
+                    alt='ls-login-image'
+                  />
+                </NavLink>
+              )}
+              {!isLoggedIn && (
+                <NavLink
+                  exact
+                  to='/sign-in-noLS'
+                  onClick={handleMenu}
                   className={({ isActive }) =>
                     isActive ? 'Sign-in5' : 'Sign-in6'
                   }

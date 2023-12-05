8a7bfd0c768cffc300d7d57ebd0b1d056c1cdb99
Gemma <johndoe@example.com>
2023-12-05 10:57:40 +0100

    changes css log in form

--- frontend/src/components/SignInOptions/SignInOptions.js
+++ frontend/src/components/SignInOptions/SignInOptions.js
@@ -3,29 +3,43 @@ import { NavLink } from 'react-router-dom'
 
 function SignInOptions () {
   return (
-    <div className='signInContainer'>
-      <div className='optionsContainer'>
-        <div className='lsContainer'>
-          <NavLink
-            exact
-            to='/sign-in'
-            className={({ isActive }) => (isActive ? 'Sign-in2' : 'Sign-in')}
-          >
-            <img
-              className='ls-login-image'
-              src='/../ls-login.png'
-              alt='ls-login-image'
-            />
-          </NavLink>
+    <div className='container'>
+      <div className='screen'>
+        <div className='screen__content'>
+          <form className='login2'>
+            <div className='login__field2'>
+              <NavLink
+                exact
+                to='/sign-in'
+                className={({ isActive }) =>
+                  isActive ? 'Sign-in2' : 'Sign-in'
+                }
+              >
+                <img
+                  className='ls-login-image'
+                  src='/../ls-login.png'
+                  alt='ls-login-image'
+                />
+              </NavLink>
+            </div>
+            <hr className='lineLogin'></hr>
+            <div className='login__field2'>
+              <NavLink
+                exact
+                to='/sign-in-noLS'
+                className={({ isActive }) =>
+                  isActive ? 'Sign-in2' : 'Sign-in'
+                }
+              >
+                <h6>Beacon Network IDP Login</h6>
+              </NavLink>
+            </div>
+          </form>
         </div>
-        <div className='noLsContainer'>
-          <NavLink
-            exact
-            to='/sign-in-noLS'
-            className={({ isActive }) => (isActive ? 'Sign-in2' : 'Sign-in')}
-          >
-            <h6>Beacon Network IDP Login</h6>
-          </NavLink>
+        <div className='screen__background'>
+          <span className='screen__background__shape screen__background__shape6'></span>
+          <span className='screen__background__shape screen__background__shape2'></span>
+          <span className='screen__background__shape screen__background__shape5'></span>
         </div>
       </div>
     </div>
